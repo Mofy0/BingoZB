3a56b5755c794513de5eea5737ef819d17812556
Mofy0 <alanbanas193@gmail.com>
2025-09-14 00:00:12 +0200

    Added smth new

---
+++ BingoZB/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+	<script >
+// Zrob to importa z jsona jesli bedzie dzialac to zrob calego jsona i wtedy zrob wyglad jak narazie git to wyglada i jutro powienienes skonczyc
+         let count = $state(0);
+        console.log(jsonData);
+        function increment() {
+            count += 1;
+            alert(count);
+        }
+    </script>
+    
+    <link rel="stylesheet" href="./src/style.css">
+    <div id="BingoDiv" class="w-128 h-128 border-5 border-indigo-600 grid grid-cols-3 text-center ">
+        <button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+    </div>
--- BingoZB/vite.config.js
+++ BingoZB/vite.config.js
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
+import json from '@rollup/plugin-json'
+
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
-		sveltekit()
+		sveltekit(),
+		json({
+    			compact: true
+  			})
 	]
 });
