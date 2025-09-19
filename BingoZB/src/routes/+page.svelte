d4ad7903975d82bad61189ce131462c3915cb18d
Mofy0 <alanbanas193@gmail.com>
2025-09-19 19:42:30 +0200

    Added new items in json and added mark fucn (i gonna change name of func later)

--- BingoZB/src/routes/+page.svelte
+++ BingoZB/src/routes/+page.svelte
@@ -3,23 +3,23 @@
     import jsondata from '../../src/json/bingocards.json';
 
         console.log(jsondata);
-        let count = $state(0);        
-        console.log();
-        function increment() {
-            count += 1;
-            alert(count);
+        function increment(e) {
+            if(e.target.classList.contains("bg-green-400")) {
+                e.target.classList.remove("bg-green-400");
+            } else {
+                e.target.classList.add("bg-green-400");
+            }
         }
-    </script>
+        </script>
     
-    <link rel="stylesheet" href="./src/style.css">
-    <div id="BingoDiv" class="w-128 h-128 border-5 border-indigo-600 grid grid-cols-3 text-center ">
-        <button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
-		<button id="BingoBT" class="shadow-md hover:bg-sky-100">Dobra cos tu bedzie</button>
+    <div id="BingoDiv" class="w-128 h-128 border-5 border-indigo-600 grid grid-cols-3 text-center text-wrap: wrap-anywhere">
+        <button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>{jsondata.cards.easy[0].text}</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment} >Dobra cos tu bedzie</button>
+		<button id="BingoBT" class="shadow-md hover:bg-sky-100" on:click={increment}>Dobra cos tu bedzie</button>
     </div>
